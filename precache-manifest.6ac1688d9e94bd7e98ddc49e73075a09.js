ea4e3d966a00bb5b205da1b3dbb4e45439d6598c
bhavesh-kreeti <bhavesh.mishra@kreeti.com>
2020-09-11 03:19:07 +0530

    Updates

---
+++ precache-manifest.6ac1688d9e94bd7e98ddc49e73075a09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89b808c902987e7e2875419125786c1",
+    "url": "/Notepad-react/index.html"
+  },
+  {
+    "revision": "6efc8b51858ac996d9a1",
+    "url": "/Notepad-react/static/css/2.1eb21bb0.chunk.css"
+  },
+  {
+    "revision": "1f0f62a57e350fed156a",
+    "url": "/Notepad-react/static/css/main.b119195f.chunk.css"
+  },
+  {
+    "revision": "6efc8b51858ac996d9a1",
+    "url": "/Notepad-react/static/js/2.dec63e21.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/Notepad-react/static/js/2.dec63e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0f62a57e350fed156a",
+    "url": "/Notepad-react/static/js/main.d12f4dc0.chunk.js"
+  },
+  {
+    "revision": "9456a3f5e9d2ace28158",
+    "url": "/Notepad-react/static/js/runtime-main.ecffd1e8.js"
+  }
+]);
